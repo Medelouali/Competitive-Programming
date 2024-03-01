027e412cf5ce3dad342380237fd5697b1d4baf78
Medelouali <Mohammedelouali1999@gmail.com>
2024-03-01 11:58:15 +0100

    Adding BFS search algo

--- src/main/java/com/adria/graphs/DfsSearch.java
+++ src/main/java/com/adria/graphs/DfsSearch.java
@@ -28,52 +28,5 @@ class DfsSearch {
         Node.dfsSearch(root);
     }
 
-    public static class Node{
-        int data;
-        boolean isVisited;
-        List<Node> neighborsNodes =new ArrayList();
 
-        Node(int data){
-            this.data=data;
-        }
-
-        Node(int data, List<Node> neighborsNodes){
-            this.data=data;
-            this.neighborsNodes = neighborsNodes;
-        }
-
-        public static void dfsSearch(Node root){
-            Stack<Node> dfsStack=new Stack<>();
-            dfsStack.push(root);
-
-            while(!dfsStack.isEmpty()){
-                Node currentNode=dfsStack.pop();
-                currentNode.processNode();
-                currentNode.visitNode();
-                for(Node node: currentNode.getNeighborsNodes()){
-                    if(!node.isVisited) dfsStack.push(node);
-                }
-            }
-        }
-
-        public int getData(){
-            return this.data;
-        }
-
-        public List<Node> getNeighborsNodes(){
-            return this.neighborsNodes;
-        }
-
-        public void visitNode(){
-            this.isVisited=true;
-        }
-
-        public void addNeiborsNodes(List<Node> nodes){
-            this.neighborsNodes.addAll(nodes);
-        }
-
-        public void processNode(){
-            System.out.println("Processing node with data "+getData());
-        }
-    }
 }
